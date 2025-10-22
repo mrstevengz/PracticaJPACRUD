4e83dd3a5741cdfdb5f9cdc410946b98bdf01e8a
gague-uam <ssequeira@uamv.edu.ni>
2025-10-22 15:37:50 -0600

    Se configuro la entidad Libro

--- PracticaJPA/src/main/java/entities/Libro.java
+++ PracticaJPA/src/main/java/entities/Libro.java
@@ -1,8 +1,31 @@
 package entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Table(name = "libros")
+@Getter
+@Setter
+@NamedQueries({
+        @NamedQuery(name = "libros", query = "SELECT l FROM Libro l"),
+})
 public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "titulo", length = 150, nullable = false)
+    private String titulo;
+
+    private int anioPub;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "autor_id", nullable = false)
+    private Autor autor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "categoria_id", nullable = false)
+    private Categoria categoria;
 
 }
