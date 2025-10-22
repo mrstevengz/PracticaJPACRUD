34b38502e5bb5d57bbfa8a12672becb8a8b6c2ff
gague-uam <ssequeira@uamv.edu.ni>
2025-10-22 15:46:07 -0600

    Se creo los paquetes interfaces y dao, y se configuro los archivos ICRUD.java y MyDao.Java con los metodos para el CRUD

---
+++ PracticaJPA/src/main/java/services/interfaces/ICRUD.java
@@ -0,0 +1,12 @@
+package services.interfaces;
+
+import java.util.List;
+
+public interface ICRUD {
+    <T> List<T> getAll(String namedQuery, Class<T> clazz);
+
+    <T>void insert(T entity);
+    <T> void update(T entity);
+    <T> void delete(T entity);
+    <T> T findById(Integer id, Class<T> clazz);
+}
