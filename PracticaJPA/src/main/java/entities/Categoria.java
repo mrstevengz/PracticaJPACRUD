767e9eacd7c958e186462fa368787167219873ba
gague-uam <ssequeira@uamv.edu.ni>
2025-10-22 15:34:55 -0600

    Se configuro la entidad Categoria

--- PracticaJPA/src/main/java/entities/Categoria.java
+++ PracticaJPA/src/main/java/entities/Categoria.java
@@ -1,4 +1,22 @@
 package entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categorias")
+@Getter
+@Setter
+@NamedQueries({
+        @NamedQuery(name = "categorias.All", query = "SELECT c FROM Categoria c"),
+})
+
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre_categoria", length = 100, nullable = false)
+    private String nombre;
 }
